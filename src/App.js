1f8c2f5a7703a5ed5352eb2e82e946eb15521d67
subhadip1203 <subhadip1203@gmail.com>
2021-10-23 19:14:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{display:'flex', backgroundColor:'yellowgreen', width:'100%' , height:'100%'}}>
+      my home page
+    </div>
+  )
+}
+
+export default App
