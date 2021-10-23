94abc05d2522a8b0ada68318f2de0243eb0fc798
Vikul Kumar <info.vikuldev@gmail.com>
2021-10-23 22:49:26 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div style={{display:'flex', backgroundColor:'yellowgreen', width:'100%' , height:'100%'}}>
-      my home page
+      Vikul's home page
     </div>
   )
 }
