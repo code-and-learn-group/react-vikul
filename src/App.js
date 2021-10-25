aa01eb2b315a4f080dfbc4ec2b09aecdc63441be
Vikul Kumar <info.vikuldev@gmail.com>
2021-10-25 12:42:20 +0530

    I have created this web form and there are some problem,why react is ignoring one input(passowrd) field kindly check and help me out with this

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import './App.scss';
+import SignIn from './Components/signIn';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import SignUp from './Components/signup';
+import ForgotPassword from './Components/forgot';
+import Sent from './Components/sent';
+import {useState} from "react";
+import Validation from './Components/validate';
+
+
 
 function App() {
+
+  const [values,setValues] = useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassowrd:""
+  })
+
+  const onChangeHandler =(e)=>{
+    setValues({
+      ...values,
+      [e.target.name] : e.target.value,
+    });
+  }
+
+  const[errors, setErrors] = useState({})
+  const onSubmitHandler =(e)=>{
+    e.preventDefault();
+    setErrors(Validation(values));
+};
+
+
   return (
-    <div style={{display:'flex', backgroundColor:'yellowgreen', width:'100%' , height:'100%'}}>
-      Vikul's home page
-    </div>
-  )
+   <Router>
+      <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <SignIn/>
+        </Route>
+        <Route exact path="/signup">
+          <SignUp onSubmitHandler={onSubmitHandler} onChangeHandler={onChangeHandler} errors={errors }values={values}/>
+        </Route>
+        <Route exact path="/forgetpassword">
+          <ForgotPassword/>
+        </Route>
+        <Route exact path="/sent">
+          <Sent/>
+        </Route>
+      </Switch>
+      </div>
+   </Router>
+  );
 }
 
-export default App
+export default App;
